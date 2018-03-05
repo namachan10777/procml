05ff1fcd17921c875f0d292cee6235679da1adcf
namachan10777 <namachan10777@gmail.com>
2018-03-06 02:00:19 +0900

    main implementation to test parser

--- compiler/src/main.ml
+++ compiler/src/main.ml
@@ -1,2 +1,9 @@
 let () =
-	Printf.printf "Hello World!"
+	let lexbuf = stdin |> Sedlexing.Utf8.from_channel |> Lexer.create_lexbuf "" in
+	let lexer () =
+		let ante_position = lexbuf.pos in
+		let token = Lexer.lex lexbuf in
+		let post_position = lexbuf.pos in
+		(token, ante_position, post_position) in
+	let result = (MenhirLib.Convert.Simplified.traditional2revised Parser.program) lexer in
+	result |> ignore
